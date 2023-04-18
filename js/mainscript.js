9e5fae0a07cfc722879be552b25178a07ce19175
artdm lab <artdm@dvc.edu>
2023-04-18 10:44:54 -0700

    css java

--- js/mainscript.js
+++ js/mainscript.js
@@ -9,8 +9,13 @@ function init() {
     // create a playlist of functions to call at certain moments in the video.
    myCues = [
         
-        { seconds: 4, callback: func1 },
-        { seconds: 10, callback: func2 },
+        { seconds: 10, callback: func1 },
+        { seconds: 36, callback: func2 },
+        { seconds: 63, callback: func3 },
+        { seconds: 64, callback: func4 },
+        { seconds: 35, callback: func5 },
+        { seconds: 107, callback: func6 },
+        { seconds: 165, callback: func7 },
     
     ];
 
@@ -27,7 +32,7 @@ function func1() {
     display.classList.remove("hide");
     setTimeout(() => {
         document.querySelector(".show").classList.toggle("hide");
-    }, 4000);
+    }, 22000);
 
 
      console.log(display);
@@ -36,6 +41,80 @@ function func1() {
 function func2() {
     let display = document.querySelector(".album");
     display.classList.remove("hide");
+    setTimeout(() => {
+        document.querySelector(".album").classList.toggle("hide");
+    }, 25000);
+    
+    console.log(display);
 }
 
+function func3() {
+    let display = document.querySelector(".notes");
+    display.classList.remove("hide");
+    setTimeout(() => {
+        document.querySelector(".notes").classList.toggle("hide");
+    }, 25000);
+    
+    console.log(display);
+}
+
+function func4() {
+    let display = document.querySelector(".piano");
+    let videoPiano = document.querySelector(".video2").muted = true;
+    display.classList.remove("hide");
+    document.querySelector(".video2").muted = true;
+    setTimeout(() => {
+        document.querySelector(".piano").classList.toggle("hide");
+    }, 25000);
+    
+    console.log(display);
+}
+
+function func5() {
+    document.body.style.background = "#242425";
+    let backgroundColor = document.querySelector(".album");
+    backgroundColor.style.background = "#242425";
+    backgroundColor.style.color = "#fff";
+    document.querySelector(".titulo").style.color = "#fff";
+}
+
+function func6() {
+    let display = document.querySelector(".oficial");
+    document.querySelector(".video").muted =true;
+    let videoOficial = document.querySelector(".video3");
+    display.classList.remove("hide");
+    
+    document.querySelector(".video3").currentTime = 171.50;
+    setTimeout(() => {
+        document.querySelector(".oficial").classList.toggle("hide");
+    }, 90000);
+    
+    console.log(display);
+}
+
+function func7() {
+    
+    document.querySelector(".video").muted =false;
+    let videoOficial = document.querySelector(".video3").muted =true;
+    
+    
+    
+    
+    console.log(display);
+}
+
+
+
+
+
+// function func3() {
+//     let display = document.querySelector(".love");
+//     display.classList.remove("hide");
+//     setTimeout(() => {
+//         document.querySelector(".love").classList.toggle("hide");
+//     }, 4000);
+    
+
+//     console.log(display);
+// }
 
